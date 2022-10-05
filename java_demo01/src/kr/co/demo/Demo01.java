ba19ac25b167500b06a12834ea1ded90eba99083
jongin <instantgogo@@naver.com>
2022-10-05 20:17:28 +0900

    출력 구문 작

--- java_demo01/src/kr/co/demo/Demo01.java
+++ java_demo01/src/kr/co/demo/Demo01.java
@@ -1,5 +1,10 @@
 package kr.co.demo;
 
 public class Demo01 {
-	asdfasdf
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
 }
