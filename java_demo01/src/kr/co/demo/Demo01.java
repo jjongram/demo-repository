7b8a38a5d2a80ed7e2197007d56815b461cf3106
jongin <instantgogo@@naver.com>
2022-10-05 21:49:37 +0900

    오늘 배운 이

--- java_demo01/src/kr/co/demo/Demo01.java
+++ java_demo01/src/kr/co/demo/Demo01.java
@@ -4,7 +4,24 @@ public class Demo01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//출력 코드 : 콘솔(터미널)에 문자열이 출력된다.
 		System.out.println("Hello World!!");
+		
+		//실행 코드의 마지막에는 반드시 세미콜론 -> ;
+		//이 있어야 한다.
+/*
+ * 블럭 주석
+ */
+		
+
+/**
+ * 
+ * 클래스나 메서드에 대한 설명을 작성하기 위한 용도로 사
+ * 하지만 블럭 주석이다.
+ * @author user2
+ * 
+ */
 	}
 
 }
