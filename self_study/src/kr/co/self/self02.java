f224beba9151d74fa8604db9407b279cb6690c17
jongin <instantgogo@@naver.com>
2022-10-07 01:48:39 +0900

    임의의 정수 만들기 -5~5

---
+++ self_study/src/kr/co/self/self02.java
@@ -0,0 +1,12 @@
+package kr.co.self;
+
+public class self02 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		for (int i = 1; i <= 20; i++) {
+			System.out.println((int)(Math.random()*11)-5);
+		}
+	}
+
+}
