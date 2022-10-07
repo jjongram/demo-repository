0015c080c2d477203d3946e95cef2614c06d97e1
jongin <instantgogo@@naver.com>
2022-10-08 02:33:45 +0900

    if조건문 문제2

---
+++ java_demo04/src/java_demo04/Exam02.java
@@ -0,0 +1,24 @@
+package java_demo04;
+
+import java.util.Scanner;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		/*
+		 * 사용자 입력과 if 문을 활용하여 사용자로부터 정수값을
+		 * 입력 받았을 때, 5의 배수 값만 출력하게 하세요.
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수값을 입력해주세요.");
+		int num = sc.nextInt();
+		
+		if(num/5 == 1) {
+			System.out.println(num);
+		}
+
+		
+	}
+
+}
