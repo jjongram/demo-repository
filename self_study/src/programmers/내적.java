3eea0402c2e33cb393ed8d388cea44cfa5d4f5d7
jjongram <instantgogo@naver.com>
2022-12-01 17:11:21 +0900

    PS

--- self_study/src/programmers/내적.java
+++ self_study/src/programmers/내적.java
@@ -5,12 +5,10 @@ public class 내적 {
 	public static void main(String[] args) {
 		int[] a = {1,2,3,4};
 		int[] b = {-3,-1,0,2};
-		int[] c = new int[a.length];
 		int answer = 0;
 		for(int i = 0; i < a.length; i++) {
 			answer += (a[i] * b[i]);
 		}
-		System.out.println(answer);
 	}
 
 }
--- self_study/src/programmers/문자열내림차순으로배치하기.java
+++ self_study/src/programmers/문자열내림차순으로배치하기.java
@@ -3,8 +3,7 @@ package programmers;
 public class 문자열내림차순으로배치하기 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
