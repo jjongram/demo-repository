c24991bca239a6461f1c0a1ffa2208da18b782db
jjongram <instantgogo@naver.com>
2022-12-09 15:48:07 +0900

    인텔리제이 설정중에 바뀐것들

--- self_study/src/kr/co/self02/self.java
+++ self_study/src/kr/co/self02/self.java
@@ -5,22 +5,23 @@ public class self {
 	public static void main(String[] args) {
 	       Class03[] menu = new Class03[3];
 	       
-	       for (int a = 0; a < menu.length; a++) {
-	          menu[a] = new Class03();
-	       }
-	       
-	       menu[0].menuName = "돈까스";
-	       menu[0].menuPrice = 7000;
-	       
-	       menu[1].menuName = "제육덮밥";
-	       menu[1].menuPrice = 9000;
-	       
-	       menu[2].menuName = "라면";
-	       menu[2].menuPrice = 4500;
-	       
-	       for (int b = 0; b < menu.length; b++) {
-	      menu[b].allMenu();
-	       }
+//	       for (int a = 0; a < menu.length; a++) {
+//	          menu[a] = new Class03();
+//	       }
+//
+//	       menu[0].menuName = "돈까스";
+//	       menu[0].menuPrice = 7000;
+//
+//	       menu[1].menuName = "제육덮밥";
+//	       menu[1].menuPrice = 9000;
+//
+//	       menu[2].menuName = "라면";
+//	       menu[2].menuPrice = 4500;
+//
+//	       for (int b = 0; b < menu.length; b++) {
+//	      menu[b].allMenu();
+//	       }
+		System.out.println(123);
 	}
 }
 
--- self_study/src/programmers/시저암호.java
+++ self_study/src/programmers/시저암호.java
@@ -5,12 +5,14 @@ public class 시저암호 {
         int n = 4;
         String answer = "";
         for(int i = 0; i < s.length(); i++) {
-            if((97 <= (int)s.charAt(i) && (int)s.charAt(i) <= 122) || (65 <= (int)s.charAt(i) && (int)s.charAt(i) <= 90)){
-                answer += (char)(((int)s.charAt(i)+n));
-            }
+                if(97 <= (int)s.charAt(i) && (int)s.charAt(i) <= 122){
+                    answer += (char)((((int)s.charAt(i)+n)-97)%26 + 97);
+            }   else if (65 <= (int)s.charAt(i) && (int)s.charAt(i) <= 90) {
+                    answer += (char)((((int)s.charAt(i)+n)-65)%26 + 65);
+                } else {
+                    answer += s.charAt(i);
+                }
         }
         System.out.println(answer);
-        System.out.println((int)'z');
-        System.out.println((int)'a');
     }
 }
